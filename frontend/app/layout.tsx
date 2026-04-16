cfe3b5c5598236ab95e4bad3b230919995e19df1
Hasnain Shabbir <hasnainshabbir186@gmail.com>
2026-04-17 00:20:34 +0500

    chore: install sonner lib

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Figtree, Manrope } from 'next/font/google';
 
 import './globals.css';
+import { Toaster } from 'sonner';
 
 const manrope = Manrope({
   subsets: ['latin'],
@@ -33,6 +34,7 @@ export default function RootLayout({
     >
       <body className={`${manrope.className} flex min-h-full flex-col`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
