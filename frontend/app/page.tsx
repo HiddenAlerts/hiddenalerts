56ca9e0f5d3bf5cc19b7e9abc165f73bd02994fc
Hasnain Shabbir <hasnainshabbir186@gmail.com>
2026-04-16 22:37:40 +0500

    chore: setup fonts and colors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <h1>Hello World</h1>
+      <h1 className="font-figtree">Hello World Figtree</h1>
+      <h1 className="font-manrope">Hello World Figtree</h1>
     </div>
   );
 }
